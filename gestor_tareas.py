df04f0452b833db43a0c5c6e78c1875177a827f9
Aaron Santos <2302042@utrivieramaya.edu.mx>
2025-03-10 21:55:30 -0500

    Gestor tareas terminado

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -60,3 +60,63 @@ class GestorTareas:
       
       # Si no encontramos la tarea con ese ID
       return "No se encontró ninguna tarea con ese ID"
+   
+   # * Metodo buscar tarea por ID
+   def buscar_tarea_porID(self, id_tarea):
+      #Recorremos el array que contiene las tareas
+      for tarea in self.tareas:
+
+         if tarea.id == id_tarea:
+
+            #Si encontramos la tarea, nos muestra el titulo y su ID
+            return f"Busqueda exitosa. La tarea es : '{tarea.titulo}', con el ID: {tarea.id}"
+      
+      return "ERROR: No se encontró ninguna tarea con ese ID"
+      
+   # * Metodo marcar tarea completada
+   def marcar_tarea_completada(self, id_tarea):
+      #Recorremos el array que contiene las tareas
+      for tarea in self.tareas:
+
+         if tarea.id == id_tarea:
+            # Si encontramos la tarea, la marcamos como completada
+            tarea.completada = True
+            return f"La tarea con ID {id_tarea} ha sido marcada como completada."
+    
+      # Si no se encuentra la tarea
+      return "ERROR: No se encontró ninguna tarea con ese ID"
+
+   # * Método para listar tareas (todas o solo pendientes)
+   def listar_tareas(self, TareasPorCompletar=False):
+      if TareasPorCompletar:
+         # Filtrar solo tareas pendientes
+         tareas_filtradas = [
+               f"ID: {tarea.id}, Título: {tarea.titulo}, ¿Completada?: {tarea.completada}"
+               for tarea in self.tareas if not tarea.completada
+         ]
+      else:
+         # Listar todas las tareas
+         tareas_filtradas = [
+               f"ID: {tarea.id}, Título: {tarea.titulo}, ¿Completada?: {tarea.completada}"
+               for tarea in self.tareas
+         ]
+      
+      if tareas_filtradas:
+         return "\n".join(tareas_filtradas)
+      return "No hay tareas registradas." if not TareasPorCompletar else "No hay tareas pendientes."
+
+   # * Metodo para filtrar las tareas por prioridad
+   def filtrar_tarea_prioridad(self, prioridad):
+
+      if prioridad not in ["baja", "normal", "alta"]:
+          return "ERROR: Prioridad no válida. Use 'baja', 'normal' o 'alta'."
+      
+      tareas_filtradas = [
+         f"ID: {tarea.id}, Título: {tarea.titulo}, ¿Completada?: {tarea.completada}, Prioridad: {tarea.prioridad}"
+         for tarea in self.tareas if tarea.prioridad == prioridad
+      ]
+
+      if tareas_filtradas:
+         return "\n".join(tareas_filtradas)
+      
+      return f"No hay tareas con prioridad '{prioridad}'."
